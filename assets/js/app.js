32a6c7ec2b68f4b51604068c8b83c3882e81a97d
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-07-09 13:43:27 -0500

    Renders x axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,13 +13,22 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 var svg = d3
-    .select("scatter")
+    .select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Function to create the xScale
+function xScale(healthData, chosenX) {
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(healthData, d => d[chosenX]) * 0.8,
+        d3.max(healthData, d => d[chosenX]) * 1.2])
+        .range([0, width]);
+    return xLinearScale
+};
 
 // Reading and parsing the csv
 d3.csv("../assets/data/data.csv").then(function (healthData) {
@@ -29,5 +38,12 @@ d3.csv("../assets/data/data.csv").then(function (healthData) {
         data.poverty = +data.poverty;
     });
 
-
+    // X axis
+    var xLinearScale = xScale(healthData, healthData.poverty);
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+ 
 });
--- commits.md
+++ commits.md
@@ -1,4 +1,6 @@
 # 07/08/2021
 - Initial commit
 - Creates file structure
+
+# 07/09/2021
 - Adds function to xScale
